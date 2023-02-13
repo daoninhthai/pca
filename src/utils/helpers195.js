9b2271c6b89cda6e335040f576daad3350027b00
daoninhthai <thaimeo1131@gmail.com>
2023-02-13 14:08:01 +0700

    fix: resolve encoding issue with UTF-8 strings

--- src/utils/helpers195.js
+++ src/utils/helpers195.js
@@ -64,3 +64,19 @@ export default {
     deepClone,
     capitalizeWords,
 };
+
+
+/**
+ * Debounce function to limit rapid invocations.
+ * @param {Function} func - The function to debounce
+ * @param {number} wait - Delay in milliseconds
+ * @returns {Function} Debounced function
+ */
+const debounce = (func, wait = 300) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+};
+
