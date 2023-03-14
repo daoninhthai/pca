298343fa427d53a4523110f5bf13e7936ff0eb79
daoninhthai <thaimeo1131@gmail.com>
2023-03-14 10:04:34 +0700

    chinh sua CSS cho responsive

--- src/main/java/com/utils/ValidationUtils177.java
+++ src/main/java/com/utils/ValidationUtils177.java
@@ -60,4 +60,16 @@ public class ValidationUtils177 {
     public static boolean isPositive(double value) {
         return value > 0;
     }
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
