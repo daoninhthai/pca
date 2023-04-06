8db1711f98b29d151e4bf4036b52d9dd6207d300
daoninhthai <thaimeo1131@gmail.com>
2023-04-06 08:57:20 +0700

    style: fix indentation and formatting

--- src/main/java/com/service/CacheService751.java
+++ src/main/java/com/service/CacheService751.java
@@ -83,4 +83,16 @@ public class CacheService751 {
             return System.currentTimeMillis() > expiresAt;
         }
     }
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
