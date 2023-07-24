1a393479d674a39e8a8962c4cad43b23732884a4
daoninhthai <thaimeo1131@gmail.com>
2023-07-24 21:47:32 +0700

    chore: update dependency versions

--- order-service/src/main/java/com/ecommerce/order/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/order/service/OrderService.java
@@ -90,4 +90,14 @@ public class OrderService {
             .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
+
+    /**
+     * Validates if the given string is not null or empty.
+     * @param value the string to validate
+     * @return true if the string has content
+     */
+    private boolean isNotEmpty(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+
 }
